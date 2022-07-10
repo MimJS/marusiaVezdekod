1a1afbbdfe8b8724107e65216d3920a6c4ccddb6
mimjs <mikhailmateevskiy@gmail.com>
2022-07-10 12:44:35 +0400

    Update indexHTTPS.js

--- 20/indexHTTPS.js
+++ 20/indexHTTPS.js
@@ -3,6 +3,7 @@ const express = require("express");
 const handleRequest = require("./handleRequest");
 const fs = require("fs");
 const https = require("https");
+const http = require("http");
 const path = require("path");
 const options = {
   key: fs.readFileSync(path.join(__dirname, "../key.key")),
@@ -12,8 +13,9 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.post("/ws", (req, res) => {
-  console.log('new req')
+  console.log("new req");
   return res.send(handleRequest(req.body));
 });
 
+http.createServer(app).listen(4001);
 https.createServer(options, app).listen(8001);
